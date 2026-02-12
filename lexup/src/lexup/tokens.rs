408631790dd751cb4c9791c4cd3cc304485392a2
Sup#2.0 <sup2point0@gmail.com>
2026-02-12 22:56:32 +0000

    dev -- add tokens

--- lexup/src/lexup/tokens.rs
+++ lexup/src/lexup/tokens.rs
@@ -1,4 +1,30 @@
+#[derive(Clone, PartialEq)]
 pub enum SupToken
 {
-    L_BRACE, R_BRACE,
+    L_PAREN,   R_PAREN,
+    L_BRACKET, R_BRACKET,
+    L_BRACE,   R_BRACE,
+
+    PIPE, DOT, COMMA,
+    PLUS, MINUS, STAR, SLASH,
+    TILDE,
+
+    IDENT(String),
+    INT(i32),
+    FLOAT(f64),
+
+    SET, LET, CONST,
+    ALT,
+    OUT,
+
+    IF, ELSE,
+    LOOP, WHILE, UNTIL,
+    TRY, EVADE, ENSURE,
+
+    FUNC, STRUCT, ARCHETYPE,
+    ACTIVATE, CREATE, EVOLVE,
+
+    AUTO, WITH,
+
+    IS,
 }
